571a7e391ba18708c09004a319579556967bd339
Ivan Arcuschin <iarcuschin@gmail.com>
2025-07-06 10:55:07 +0000

    SAEs for Llama 8B layer 6 - n_clusters 21 to 40

--- train-saes/run.sh
+++ train-saes/run.sh
@@ -1,4 +1,4 @@
-MIN_CLUSTERS=21
+MIN_CLUSTERS=4
 MAX_CLUSTERS=40
 N_EXAMPLES=100000  # all responses
 CLUSTERING_METHODS="gmm pca_gmm spherical_kmeans pca_kmeans agglomerative pca_agglomerative sae_topk"
@@ -12,7 +12,7 @@ CLUSTERING_METHODS="gmm pca_gmm spherical_kmeans pca_kmeans agglomerative pca_ag
 for LAYER in 6 10 14 18 22 26; do
     python ablate_clustering.py --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B --layer $LAYER --min_clusters $MIN_CLUSTERS --max_clusters $MAX_CLUSTERS --n_examples $N_EXAMPLES --clustering_methods sae_topk
     # python re_evaluate_trained_clustering.py --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B --layer $LAYER --min_clusters $MIN_CLUSTERS --max_clusters $MAX_CLUSTERS --n_examples $N_EXAMPLES --clustering_methods sae_topk
-    python visualize_results.py --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B --layer $LAYER
+    python visualize_results.py --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B --layer $LAYER --min_clusters $MIN_CLUSTERS --max_clusters $MAX_CLUSTERS
     python visualize_comparison.py --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B --layer $LAYER
 done
 
