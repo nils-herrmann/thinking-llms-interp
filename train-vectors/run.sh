8cc650da230028b9598956a8f8f0f489570acc98
Ivan Arcuschin <iarcuschin@gmail.com>
2025-07-13 01:12:37 +0000

    Minor change

--- train-vectors/run.sh
+++ train-vectors/run.sh
@@ -2,3 +2,5 @@
 for cluster in {0..19}; do
     python optimize_steering_vectors.py --model meta-llama/Llama-3.1-8B --layer 6 --has_bos_token True --steering_vector_idx $cluster --lr "5e-2"
 done
+
+python visualize_vector_losses.py --model meta-llama/Llama-3.1-8B --layer 6 --n_clusters 20
