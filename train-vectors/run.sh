52dc33d13df6bb7691224b735006e94610c5f31a
Ivan Arcuschin <iarcuschin@gmail.com>
2025-07-26 22:28:43 +0000

    Minor change

--- train-vectors/run.sh
+++ train-vectors/run.sh
@@ -1,26 +1,25 @@
-python optimize_steering_vectors.py \
-    --model meta-llama/Llama-3.1-8B \
-    --max_iters 50 \
-    --n_training_examples 2048 \
-    --minibatch_size 4 \
-    --layer 12 \
-    --steering_vector_idx -1 \
-    --lr "1e-2" \
-    --use_activation_perplexity_selection
+# python optimize_steering_vectors.py \
+#     --model meta-llama/Llama-3.1-8B \
+#     --max_iters 50 \
+#     --n_training_examples 2048 \
+#     --minibatch_size 8 \
+#     --layer 12 \
+#     --steering_vector_idx -1 \
+#     --lr "1e-2" \
+#     --use_activation_perplexity_selection
 
 
-for cluster in {0..14}; do
+for cluster in "$@"; do
     echo "Processing cluster: $cluster"
     python optimize_steering_vectors.py \
         --model meta-llama/Llama-3.1-8B \
         --max_iters 50 \
-        --n_training_examples 128 \
-        --minibatch_size 4 \
+        --n_training_examples 2048 \
+        --minibatch_size 8 \
         --layer 12 \
         --steering_vector_idx $cluster \
         --lr "1e-2" \
         --use_activation_perplexity_selection
-        #--use_wandb
 done
 
 # python visualize_vector_losses.py --model meta-llama/Llama-3.1-8B
