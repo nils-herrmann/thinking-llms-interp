d6d8356d165e778259f355a8de78b69397e93ad9
Ivan Arcuschin <iarcuschin@gmail.com>
2025-08-21 19:36:13 -0300

    Minor fix

--- generate-responses/run.sh
+++ generate-responses/run.sh
@@ -1,8 +1,8 @@
 # python generate_responses.py --model meta-llama/Llama-3.1-8B --save_every 1 --max_tokens 1000 --batch_size 64 --is_base_model
 # python generate_responses.py --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B --save_every 1 --max_tokens 1000 --batch_size 64
 
-python generate_responses.py --model qwen/Qwen-1.5B --save_every 1 --max_tokens 1000 --batch_size 64 --is_base_model
+python generate_responses.py --model qwen/Qwen2.5-1.5B --save_every 1 --max_tokens 1000 --batch_size 64 --is_base_model
 python generate_responses.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --save_every 1 --max_tokens 1000 --batch_size 64
 
-python generate_responses.py --model qwen/Qwen-14B --save_every 1 --max_tokens 1000 --batch_size 64 --is_base_model
-python generate_responses.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.4B --save_every 1 --max_tokens 1000 --batch_size 64
+python generate_responses.py --model qwen/Qwen2.5-14B --save_every 1 --max_tokens 1000 --batch_size 64 --is_base_model
+python generate_responses.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-14B --save_every 1 --max_tokens 1000 --batch_size 64
