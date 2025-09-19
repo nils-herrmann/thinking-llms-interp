c07af9d506cf34ee8ad94eafb8ba45f0130a76c9
Ivan Arcuschin <iarcuschin@gmail.com>
2025-09-19 19:29:53 +0000

    Minor change

--- generate-responses/run.sh
+++ generate-responses/run.sh
@@ -9,3 +9,6 @@
 
 # python generate_responses.py --model qwen/Qwen2.5-32B --save_every 1 --max_tokens 1000 --is_base_model
 # python generate_responses.py --model Qwen/QwQ-32B --save_every 1 --max_tokens 1000 --engine vllm
+
+# python generate_responses.py --model meta-llama/Llama-3.3-70B-Instruct --save_every 1 --max_tokens 1000 --is_base_model
+python generate_responses.py --model deepseek-ai/DeepSeek-R1-Distill-Llama-70B --save_every 1 --max_tokens 1000
